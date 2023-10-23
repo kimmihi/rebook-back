ee414c3b6a58bafcaf4eeb6033b15edc388cd128
kimmeh <mhko0411@gmail.com>
2023-10-23 21:49:58 +0900

    회원가입 dto

---
+++ back/src/auth/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('/signup')
+  signUp(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return null;
+  }
+}
---
+++ back/src/auth/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @Matches(
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i,
+    {
+      message: 'not email',
+    },
+  )
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  password: string;
+}
