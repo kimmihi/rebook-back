bddd0001f3d6a1bb78c01fac908eabe516cc587a
kimmeh <mhko0411@gmail.com>
2023-10-26 20:33:40 +0900

    feat: user entity & repository

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signUp(@Body(ValidationPipe) createUserDto: CreateUserDto) {
-    return null;
+  signUp(@Body(ValidationPipe) createUserDto: CreateUserDto): Promise<User> {
+    return this.authService.createUser(createUserDto);
   }
 }
--- back/src/auth/auth.service.ts
+++ back/src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { username, password } = createUserDto;
+    const newUser = this.userRepository.create({ username, password });
+
+    try {
+      await this.userRepository.save(newUser);
+    } catch (e) {
+      if (e.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+
+    return newUser;
+  }
+}
