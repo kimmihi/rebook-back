0c7accf41e403293682423ad8e67727a05eec43a
kimmeh <mhko0411@gmail.com>
2023-11-17 14:36:12 +0900

    fix: review service 초기화

--- back/src/reviews/reviews.service.ts
+++ back/src/reviews/reviews.service.ts
@@ -1,40 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
-import { Review } from './review.entity';
+import { ReviewEntity } from './review.entity';
 import { Repository } from 'typeorm';
-import { CreateReviewDto } from './dto/create-review.dto';
-import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class ReviewsService {
   constructor(
-    @InjectRepository(Review)
-    private reviewRepository: Repository<Review>,
+    @InjectRepository(ReviewEntity)
+    private reviewRepository: Repository<ReviewEntity>,
   ) {}
-
-  async getAllReviewsByUser(user: User): Promise<Review[]> {
-    const query = this.reviewRepository.createQueryBuilder('review');
-    query.where('review.userId = :userId', { userId: user.id });
-
-    const reviewList = await query.getMany();
-    return reviewList;
-  }
-
-  async createReview(
-    createReviewDto: CreateReviewDto,
-    user: User,
-  ): Promise<Review> {
-    const { title, content, book } = createReviewDto;
-
-    const newReview = this.reviewRepository.create({
-      title,
-      content,
-      book,
-      user,
-    });
-
-    await this.reviewRepository.save(newReview);
-    return newReview;
-  }
 }
